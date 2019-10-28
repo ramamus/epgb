5793ca2c48ca927bee99cba0556cbc0280788673
Satish Ramamurthi <satish.ramamurthi@target.com>
2019-10-27 21:59:38 -0500

    added basic template

--- src/App.js
+++ src/App.js
@@ -1,45 +1,160 @@
 import React, { Component } from 'react';
-import { Button } from 'primereact/button';
-import logo from './logo.png';
-import './App.css';
+import classNames from 'classnames';
+import { AppTopbar } from './AppTopbar';
+import { AppMenu } from './AppMenu';
+// import { Route } from 'react-router-dom';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 import './layout/layout.scss';
+import './App.scss';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { count: 0 };
-    this.increment = this.increment.bind(this);
+    this.state = {
+      layoutMode: 'static',
+      layoutColorMode: 'dark',
+      staticMenuInactive: false,
+      overlayMenuActive: false,
+      mobileMenuActive: false
+    };
+
+    this.onWrapperClick = this.onWrapperClick.bind(this);
+    this.onToggleMenu = this.onToggleMenu.bind(this);
+    this.onSidebarClick = this.onSidebarClick.bind(this);
+    this.onMenuItemClick = this.onMenuItemClick.bind(this);
+    this.createMenu();
   }
 
-  increment() {
-    this.setState({ count: this.state.count + 1 });
+  onWrapperClick(event) {
+    if (!this.menuClick) {
+      this.setState({
+        overlayMenuActive: false,
+        mobileMenuActive: false
+      });
+    }
+
+    this.menuClick = false;
+  }
+
+  onToggleMenu(event) {
+    this.menuClick = true;
+
+    if (this.isDesktop()) {
+      if (this.state.layoutMode === 'overlay') {
+        this.setState({
+          overlayMenuActive: !this.state.overlayMenuActive
+        });
+      } else if (this.state.layoutMode === 'static') {
+        this.setState({
+          staticMenuInactive: !this.state.staticMenuInactive
+        });
+      }
+    } else {
+      const mobileMenuActive = this.state.mobileMenuActive;
+      this.setState({
+        mobileMenuActive: !mobileMenuActive
+      });
+    }
+
+    event.preventDefault();
+  }
+
+  onSidebarClick(event) {
+    this.menuClick = true;
+  }
+
+  onMenuItemClick(event) {
+    if (!event.item.items) {
+      this.setState({
+        overlayMenuActive: false,
+        mobileMenuActive: false
+      });
+    }
+  }
+
+  createMenu() {
+    this.menu = [
+      {
+        label: 'Players',
+        icon: 'pi pi-fw pi-user',
+        command: () => {
+          window.location = '#/';
+        }
+      },
+      {
+        label: 'Reports',
+        icon: 'pi pi-fw pi-table',
+        command: () => {
+          window.location = '#/reports';
+        }
+      }];
+  }
+
+  addClass(element, className) {
+    if (element.classList) element.classList.add(className);
+    else element.className += ' ' + className;
+  }
+
+  removeClass(element, className) {
+    if (element.classList) element.classList.remove(className);
+    else
+      element.className = element.className.replace(
+        new RegExp(
+          '(^|\\b)' + className.split(' ').join('|') + '(\\b|$)',
+          'gi'
+        ),
+        ' '
+      );
+  }
+
+  isDesktop() {
+    return window.innerWidth > 1024;
+  }
+
+  componentDidUpdate() {
+    if (this.state.mobileMenuActive)
+      this.addClass(document.body, 'body-overflow-hidden');
+    else this.removeClass(document.body, 'body-overflow-hidden');
   }
 
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to PrimeReact</h2>
-        </div>
-        <div className="p-col-12 p-lg-4">
-          <div className="card summary">
-            <span className="title">Attendence</span>
-            <span className="detail">Players Checkedin</span>
-            <span className="count visitors">{1}</span>
-          </div>
-        </div>
-        <div className="App-intro">
-          <Button label="Click" icon="pi pi-check" onClick={this.increment} />
+    const wrapperClass = classNames('layout-wrapper', {
+      'layout-overlay': this.state.layoutMode === 'overlay',
+      'layout-static': this.state.layoutMode === 'static',
+      'layout-static-sidebar-inactive':
+        this.state.staticMenuInactive && this.state.layoutMode === 'static',
+      'layout-overlay-sidebar-active':
+        this.state.overlayMenuActive && this.state.layoutMode === 'overlay',
+      'layout-mobile-sidebar-active': this.state.mobileMenuActive
+    });
 
-          <p>Number of Clicks: {this.state.count}</p>
+    const sidebarClassName = classNames('layout-sidebar', {
+      'layout-sidebar-dark': this.state.layoutColorMode === 'dark',
+      'layout-sidebar-light': this.state.layoutColorMode === 'light'
+    });
+
+    return (
+      <div className={wrapperClass} onClick={this.onWrapperClick}>
+        <AppTopbar onToggleMenu={this.onToggleMenu} />
+
+        <div
+          ref={el => (this.sidebar = el)}
+          className={sidebarClassName}
+          onClick={this.onSidebarClick}
+        >
+          <AppMenu model={this.menu} onMenuItemClick={this.onMenuItemClick} />
         </div>
+        <div className="layout-main">
+          {/* <Route path="/" exact component={Players} /> */}
+          {/* <Route path="/reports" component={Reports} /> */}
+        </div>
+        <div className="layout-mask"></div>
       </div>
     );
   }
