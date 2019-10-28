ce1959772f5bb068c703a857edec1ba9a0056a2a
Satish Ramamurthi <satish.ramamurthi@target.com>
2019-10-27 22:18:53 -0500

    redid master

--- src/pages/Players/index.js
+++ src/pages/Players/index.js
@@ -1,25 +1,19 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, {useState} from 'react';
+import { Panel } from 'primereact/panel';
+import { InputSwitch } from 'primereact/inputswitch';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Chart } from 'primereact/chart';
 import { toArray } from '../../util/reshape';
-import PlayersHotloaders from '../../components/hotloaders/PlayersHotloaders';
+import { connect } from 'react-redux';
 import { requestUpdatePlayer } from '../../ducks/players';
-import {
-  Button,
-  Badge,
-  Container,
-  Col,
-  Form,
-  Row,
-  InputGroup
-} from 'react-bootstrap';
-import styled from 'styled-components';
-import colors from '../../util/colors';
+import PlayersHotloaders from '../../components/hotloaders/PlayersHotloaders';
 
 export const Players = ({ players, _requestUpdatePlayer }) => {
   const [selectedValue, setSelectedValue] = useState('');
-  const handleSearch = e => setSelectedValue(e.target.value);
-  const handleChecked = id => {
-    const updatedPlayer = { ...players[id], checkedin: !players[id].checkedin };
+  const handleChange = e => setSelectedValue(e.target.value);
+  const handleCheckin = e => {
+    const updatedPlayer = { ...players[e.target.id], checkedin: !players[e.target.id].checkedin };
     _requestUpdatePlayer(updatedPlayer);
   };
   const filterByAnyValue = (arrayOfObjects, searchTeam) => {
@@ -38,75 +32,146 @@ export const Players = ({ players, _requestUpdatePlayer }) => {
       )
     );
   };
+  const groupByGrade =
+    players &&
+    Object.keys(players).length !== 0 &&
+    toArray(players)
+      .filter(player => player.checkedin === true)
+      .reduce((acc, it) => {
+        acc[it.grade] = acc[it.grade] + 1 || 1;
+        return acc;
+      }, {});
+  const groupByBelongTo =
+    players &&
+    Object.keys(players).length !== 0 &&
+    toArray(players)
+      .filter(player => player.checkedin === true)
+      .reduce((acc, it) => {
+        acc[it.belongto] = acc[it.belongto] + 1 || 1;
+        return acc;
+      }, {});
+  const totalAttending =
+    players &&
+    Object.keys(players).length !== 0 &&
+    toArray(players).filter(({ checkedin }) => checkedin === true).length;
+  const polarData = {
+    datasets: [
+      {
+        data: [
+          groupByGrade[4] || 0,
+          groupByGrade[5] || 0,
+          groupByGrade[6] || 0,
+          groupByGrade[7] || 0,
+          groupByGrade[8] || 0
+        ],
+        backgroundColor: [
+          '#FFC107',
+          '#03A9F4',
+          '#4CAF50',
+          '#E91E63',
+          '#9C27B0'
+        ],
+        label: 'Grade'
+      }
+    ],
+    labels: ['4th', '5th', '6th', '7th', '8th']
+  };
+  const pieData = {
+    labels: ['Travel', 'InHouse'],
+    datasets: [
+      {
+        data: [groupByBelongTo['TRAVEL'] || 0, groupByBelongTo['INHOUSE'] || 0],
+        backgroundColor: ['#FFC107', '#03A9F4'],
+        hoverBackgroundColor: ['#FFE082', '#81D4FA']
+      }
+    ]
+  };
   return (
-    <Container
-      style={{
-        margin: 'auto 31px auto 31px',
-        width: 'auto',
-        backgroundColor: 'white'
-      }}
-    >
+    <div className="p-grid p-fluid dashboard">
       <PlayersHotloaders />
-      <hr />
-      <Row>
-        <Col xs={6} md={4}>
-          <Form.Control
-            size="lg"
-            type="text"
-            placeholder="Search by name, grade, team ..."
-            onChange={handleSearch}
-          />
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col xs={3} md={4}>
-          <Button variant="primary">
-            Attendence{' '}
-            <Badge variant="light">
-              {players &&
-                Object.keys(players).length !== 0 &&
-                toArray(players).filter(player => player.checkedin === true)
-                  .length}
-            </Badge>
-          </Button>
-        </Col>
-        <Row>
-          {players &&
-            Object.keys(players).length !== 0 &&
-            filterByAnyValue(players, selectedValue).map(
-              ({ firstname, lastname, grade, team, checkedin, id }) => {
-                return (
-                  <Col xs={6} md={3}>
-                    <InfoCard
-                      className="category-details-information-card"
-                      width="auto"
-                      key={id}
-                    >
-                      <CardTitle>{stndrdth(grade)} Grade</CardTitle>
-                      <Row>
-                        <Col md={9}>
-                          <Label>{team}</Label>
-                          <InputGroup className="mb-3">
-                            <InputGroup.Checkbox
-                              checked={checkedin}
-                              aria-label="Checkbox for following text input"
-                              onChange={() => handleChecked(id)}
-                            />
-                            <InfoText className="name">
-                              {firstname} {lastname}
-                            </InfoText>
-                          </InputGroup>
-                        </Col>
-                      </Row>
-                    </InfoCard>
-                  </Col>
-                );
-              }
-            )}
-        </Row>
-      </Row>
-    </Container>
+      <div className="p-col-12 p-lg-4">
+        <div className="card summary">
+          <span className="title">Attendence</span>
+          <span className="detail">Players Checkedin</span>
+          <span className="count visitors">{totalAttending}</span>
+        </div>
+      </div>
+      <div className="p-col-12 p-md-6 p-xl-3">
+        <div className="highlight-box">
+          <div
+            className="initials"
+            style={{ backgroundColor: '#007be5', color: '#00448f' }}
+          >
+            <span>TR</span>
+          </div>
+          <div className="highlight-details ">
+            <i className="pi pi-briefcase" />
+            <span>In Attendence</span>
+            <span className="count">{groupByBelongTo['TRAVEL'] || 0}</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-col-12 p-md-6 p-xl-3">
+        <div className="highlight-box">
+          <div
+            className="initials"
+            style={{ backgroundColor: '#ef6262', color: '#a83d3b' }}
+          >
+            <span>IN</span>
+          </div>
+          <div className="highlight-details ">
+            <i className="pi pi-home" />
+            <span>In Attendence</span>
+            <span className="count">{groupByBelongTo['INHOUSE'] || 0}</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-col-12 p-lg-4 contacts">
+        <div className="p-col-12 p-md-6">
+          <div className="p-inputgroup">
+            <InputText placeholder="Search" onChange={handleChange} />
+            <Button icon="pi pi-search" />
+          </div>
+        </div>
+        <Panel header="Players">
+          <ul>
+            {players &&
+              Object.keys(players).length !== 0 &&
+              filterByAnyValue(players, selectedValue).map(
+                ({ firstname, lastname, grade, team, checkedin, id }) => {
+                  return (
+                    <li>
+                      <button className="p-link">
+                        <InputSwitch
+                          id={id}
+                          checked={checkedin}
+                          onChange={event => handleCheckin(event)}
+                        />
+                        <span className="name">
+                          {firstname} {lastname}
+                        </span>
+                        <span className="email">
+                          {grade} {team}
+                        </span>
+                      </button>
+                    </li>
+                  );
+                }
+              )}
+          </ul>
+        </Panel>
+      </div>
+      <div className="p-col-12 p-lg-6">
+        <div className="card">
+          <h1 className="centerText">Pie Chart</h1>
+          <Chart type="pie" data={pieData} height="150" />
+        </div>
+        <div className="card">
+          <h1 className="centerText">Polar Area Chart</h1>
+          <Chart type="polarArea" data={polarData} height="150" />
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -118,44 +183,3 @@ export default connect(
     }
   })
 )(Players);
-
-function stndrdth(number) {
-  const pr = new Intl.PluralRules('en-US', { type: 'ordinal' });
-  const ordinals = {
-    one: 'st',
-    two: 'nd',
-    few: 'rd',
-    many: 'th',
-    zero: 'th',
-    other: 'th'
-  };
-  return `${number}${ordinals[pr.select(number)]}`;
-}
-
-export const InfoCard = styled.div`
-  margin: 1em auto;
-  background-color: ${colors.backgroundAlt};
-  padding: 20px;
-`;
-InfoCard.displayName = 'InfoCard';
-export const CardTitle = styled.h4`
-  height: 14px;
-  color: #626262;
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 14px;
-`;
-CardTitle.displayName = 'CardTitle';
-export const Label = styled(CardTitle)`
-  height: 15px;
-  font-weight: 300;
-  line-height: 15px;
-`;
-Label.displayName = 'Label';
-export const InfoText = styled(CardTitle)`
-  color: #000000;
-  font-size: 18px;
-  line-height: 22px;
-  margin: 3px;
-`;
-InfoText.displayName = 'InfoText';
--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import PlayersHotloaders from '../../components/hotloaders/PlayersHotloaders';
+import { toArray } from '../../util/reshape';
 
-export const Reports = () => {
-  return <h1>Hello Reports</h1>;
+const Reports = ({ players }) => {
+  const attending =
+    players &&
+    Object.keys(players).length !== 0 &&
+    toArray(players).filter(({ checkedin }) => checkedin === true);
+
+  const notAttending =
+    players &&
+    Object.keys(players).length !== 0 &&
+    toArray(players).filter(({ checkedin }) => checkedin === false);
+  return (
+    <div>
+      <PlayersHotloaders />
+      <div className="p-col-12 p-lg-6">
+        <div className="card">
+          <h1 style={{ fontSize: '16px' }}>Players Checked In</h1>
+          <DataTable
+            value={attending}
+            style={{ marginBottom: '20px' }}
+            responsive={true}
+          >
+            <Column field="firstname" header="First Name" sortable={true} />
+            <Column field="lastname" header="Last Name" sortable={true} />
+            <Column field="grade" header="Grade" sortable={true} />
+            <Column field="team" header="Team" sortable={true} />
+            <Column field="belongto" header="Plays For" sortable={true} />
+          </DataTable>
+        </div>
+      </div>
+      <div className="p-col-12 p-lg-6">
+        <PlayersHotloaders />
+        <div className="card">
+          <h1 style={{ fontSize: '16px' }}>Players Not Checked In</h1>
+          <DataTable
+            value={notAttending}
+            style={{ marginBottom: '20px' }}
+            responsive={true}
+          >
+            <Column field="firstname" header="First Name" sortable={true} />
+            <Column field="lastname" header="Last Name" sortable={true} />
+            <Column field="grade" header="Grade" sortable={true} />
+            <Column field="team" header="Team" sortable={true} />
+            <Column field="belongto" header="Plays For" sortable={true} />
+          </DataTable>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Reports;
+export default connect(({ players }) => ({ players }))(Reports);
